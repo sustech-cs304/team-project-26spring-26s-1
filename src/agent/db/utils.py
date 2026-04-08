97ae2e66c7eb1307565219c63b5250cde7c87aa6
Haixin Xu <jerryxucs@gmail.com>
2026-04-08 22:46:33 +0800

    backend - use interrupts for user input & inplement HITL parsing

--- src/agent/db/utils.py
+++ src/agent/db/utils.py
@@ -4,10 +4,16 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from uuid import uuid4
 import datetime as dt
     
-async def db_get_message_by_langchain_id(session_factory: async_sessionmaker, langchain_id: str) -> db_models.Message | None:
+async def db_get_message_by_langchain_id(session_factory: async_sessionmaker, conversation_id: str, langchain_id: str) -> db_models.Message | None:
     async with session_factory() as session:
         session: AsyncSession
-        result = await session.execute(select(db_models.Message).where(db_models.Message.langchain_id == langchain_id))
+        result = await session.execute(
+            select(db_models.Message)
+                .where(db_models.Message.conversation_id == conversation_id)
+                .where(db_models.Message.langchain_id == langchain_id)
+                .order_by(db_models.Message.seq.desc())
+                .limit(1)
+        )
         return result.scalars().first()
     
 async def db_update_message(session_factory: async_sessionmaker, conversation_id: str, content: str, message_id: str | None = None, langchain_id: str | None = None, attachments: list[str] = [], checkpoint_id: str | None = None) -> str:
@@ -25,7 +31,18 @@ async def db_update_message(session_factory: async_sessionmaker, conversation_id
         # updates["attachments"] = attachments
         if langchain_id:
             updates["langchain_id"] = langchain_id
-            
+
+        async def get_last_seq():
+            """allocate a new sequence number"""
+            _result = await session.execute(
+                select(db_models.Message.seq)
+                    .where(db_models.Message.conversation_id == conversation_id)
+                    .order_by(db_models.Message.seq.desc())
+                    .limit(1)
+            )
+            last_seq = _result.scalar_one_or_none() or 0
+            return last_seq
+
         if not message_id:
             if not langchain_id:
                 raise ValueError("Either message_id or langchain_id must be provided")
@@ -37,31 +54,40 @@ async def db_update_message(session_factory: async_sessionmaker, conversation_id
             _result = _result.scalars().first()
             if not _result: # assign a new message_id
                 message_id = str(uuid4())
-                # allocate a new sequence number
-                _result = await session.execute(
-                    select(db_models.Message.seq)
-                        .where(db_models.Message.conversation_id == conversation_id)
-                        .order_by(db_models.Message.seq.desc())
-                        .limit(1)
-                )
-                last_seq = _result.scalar_one_or_none() or 0
-                
                 
                 updates["id"] = message_id
-                updates["seq"] = last_seq + 1
+                updates["seq"] = (await get_last_seq()) + 1
                 updates["conversation_id"] = conversation_id     
                 updates["created_at"] = dt.datetime.now(dt.timezone.utc) # new creation timestamp    
                 await session.execute(
                     insert(db_models.Message).values(updates)
                 )
+                
+                await session.commit()
+                return message_id
             else:
                 message_id = _result.id
+                found = True
+        else:
+            message_row = await session.execute(
+                select(db_models.Message).where(db_models.Message.id == message_id)
+            )
+            found = message_row.scalar_one_or_none() is not None
             
-        await session.execute(
-            update(db_models.Message)
-                .where(db_models.Message.id == message_id)
-                .values(updates)
-        )
+        if not found:
+            updates["id"] = message_id
+            updates["seq"] = (await get_last_seq()) + 1
+            updates["conversation_id"] = conversation_id     
+            updates["created_at"] = dt.datetime.now(dt.timezone.utc) # new creation timestamp    
+            await session.execute(
+                insert(db_models.Message).values(updates)
+            )
+        else:
+            await session.execute(
+                update(db_models.Message)
+                    .where(db_models.Message.id == message_id)
+                    .values(updates)
+            )
             
         await session.commit()
         return message_id
