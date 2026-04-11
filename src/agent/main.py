414d62e173edaf59cb6dd2d3c56eb1d7a5ece79e
Haixin Xu <jerryxucs@gmail.com>
2026-04-11 12:16:33 +0800

    backend - implement onebot integration with access control

--- src/agent/main.py
+++ src/agent/main.py
@@ -41,6 +41,10 @@ async def lifespan(app: fastapi.FastAPI):
 	app.state.engine = engine
 	app.state.async_session = async_session
 	app.state.ConversationRunner = ConversationRunner(graph, async_session, config)
+<<<<<<< HEAD
+=======
+	app.state.FileRunner = FileRunner(async_session, config)
+>>>>>>> 0a90f11... backend - implement onebot integration with access control
 	app.state.OneBotHub = OneBotHub(async_session, graph, app.state.ConversationRunner, config.onebot)
 	app.state.config = config
 	app.state.graph = graph
