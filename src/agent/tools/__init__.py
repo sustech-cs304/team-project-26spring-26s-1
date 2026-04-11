f09cf5a8f9126c8858de032999064fedd1fefe76
xciphand <3070073429@qq.com>
2026-04-11 12:26:32 +0800

    feat(backend): webtools

--- src/agent/tools/__init__.py
+++ src/agent/tools/__init__.py
@@ -7,11 +7,14 @@ class ToolArtifact(TypedDict):
     break_agent_loop: bool
 
 from .core_memory import core_memory_insert
+from .web_tools import webfetch, websearch
 from .code_interpreter import python_interpreter
 
 tools = [
     core_memory_insert,
-    python_interpreter
+    python_interpreter,
+    webfetch,
+    websearch,
 ]
 
 tool_node = ToolNode(tools=tools)
